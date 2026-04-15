a5939ae619efd76b004105d0c7089919c5f725f8
busemesee <147412600+busemesee@users.noreply.github.com>
2026-04-15 11:58:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nextcent_landing_page
+minimal landing page design
---
+++ assets/main.js
@@ -0,0 +1,11 @@
+new Swiper('.hero_swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+});
